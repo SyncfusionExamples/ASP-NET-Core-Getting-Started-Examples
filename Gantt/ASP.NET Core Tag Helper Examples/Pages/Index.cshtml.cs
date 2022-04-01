af52a9a86b53f39ae440dd9448d0d829e852a044
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-04-01 17:47:38 +0530

    Prepared getting started sample for ASP.NET Core Gantt Control

---
+++ Gantt/ASP.NET Core Tag Helper Examples/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GanttSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+    public class GanttDataSource
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public int[] ResourceId { get; set; }
+        public string Predecessor { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int? Duration { get; set; }
+        public int Progress { get; set; }
+        public List<GanttDataSource> SubTasks { get; set; }
+    }
+    public class GanttResources
+    {
+        public int ResourceId { get; set; }
+        public string ResourceName { get; set; }
+    }
+}
