f41d8377bcfa2922983efc25fc44fa9593784574
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-04-07 18:19:03 +0530

    Prepared getting started sample for ASP.NET Core Maps control

---
+++ Maps/ASP.NET Core Tag Helper Examples/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MapSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+    //public class MapsData
+    //{
+    //    //public MapsData(string countryName, string memberData)
+    //    //{
+    //    //    this.CountryName = countryName;
+    //    //    this.Membership = memberData;
+    //    //}
+    //    public string Country { get; set; }
+    //    public string Membership { get; set; }
+    //}
+}
