db1877346bd2e04e4028f66db3d7a679526b20ac
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-04-08 17:04:53 +0530

    Prepared samples for ASP.NET Core Avatar, Carousel, FileManager and StockChart controls

---
+++ FileManager/ASP.NET Core Tag Helper Examples/Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Newtonsoft.Json;
+
+//File Manager's base functions are available in the below package
+using Syncfusion.EJ2.FileManager.Base;
+//File Manager's operations are available in the below package
+using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public PhysicalFileProvider operation;
+        public string basePath;
+        string root = "wwwroot\\Files";
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            this.operation = new PhysicalFileProvider();
+            this.operation.RootFolder(this.basePath + "\\" + this.root);
+        }
+
+        public object FileOperations([FromBody] FileManagerDirectoryContent args)
+        {
+            var fullPath = (this.basePath + "\\" + this.root + args.Path).Replace("/", "\\");
+            if (args.Action == "delete" || args.Action == "rename")
+            {
+                if ((args.TargetPath == null) && (args.Path == ""))
+                {
+                    FileManagerResponse response = new FileManagerResponse();
+                    response.Error = new ErrorDetails { Code = "401", Message = "Restricted to modify the root folder." };
+                    return this.operation.ToCamelCase(response);
+                }
+            }
+            switch (args.Action)
+            {
+                case "read":
+                    // reads the file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                case "delete":
+                    // deletes the selected file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names));
+                case "copy":
+                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "move":
+                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "details":
+                    // gets the details of the selected file(s) or folder(s).
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
+                case "create":
+                    // creates a new folder in a given path.
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name));
+                case "search":
+                    // gets the list of file(s) or folder(s) from a given path based on the searched key string.
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                case "rename":
+                    // renames a file or folder.
+                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName));
+            }
+            return null;
+        }
+
+        // uploads the file(s) into a specified path
+        public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action)
+        {
+            FileManagerResponse uploadResponse;
+            uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+        }
+
+        // downloads the selected file(s) and folder(s)
+        public IActionResult Download(string downloadInput)
+        {
+            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            return operation.Download(args.Path, args.Names, args.Data);
+        }
+
+        // gets the image(s) from the given path
+        public IActionResult GetImage(FileManagerDirectoryContent args)
+        {
+            return this.operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
